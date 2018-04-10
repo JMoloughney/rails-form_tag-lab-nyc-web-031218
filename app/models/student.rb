338f526d01500110a6934a226aeb0a53b7f30d85
JMoloughney <github email address>
2018-04-10 11:04:39 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,19 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end
+
 end
